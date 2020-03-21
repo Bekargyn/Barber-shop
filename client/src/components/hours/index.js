cc102743fc9597a003554fa9e6347bb52b8cb8e2
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-03-21 08:41:41 -0500

    complited hours folder react

--- client/src/components/hours/index.js
+++ client/src/components/hours/index.js
@@ -2,5 +2,62 @@ import React from "react";
 import "./style.css";
 
 export const Hours = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div id="hours">
+      <div className="row">
+        <div className="col-12">
+          229 Skokie Valley road. Highland Park, Illinois 888-888-8888
+        </div>
+      </div>
+      <div className="container" id="container">
+        <div className="row">
+          <div className="col-12">
+            <h2>Operating Hours</h2>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <table>
+                <tr>
+                  <td>MONDAY</td>
+                  <td>8:30AM-6PM</td>
+                </tr>
+                <tr>
+                  <td>TUESDAY</td>
+                  <td>8:30AM-6PM</td>
+                </tr>
+                <tr>
+                  <td>WEDNESDAY</td>
+                  <td>8:30AM-8PM</td>
+                </tr>
+                <tr>
+                  <td>THURSDAY</td>
+                  <td>8:30AM-6PM</td>
+                </tr>
+                <tr>
+                  <td>FRIDAY</td>
+                  <td>8:30AM-6PM</td>
+                </tr>
+                <tr>
+                  <td>SATURDAY</td>
+                  <td>8:00AM-5PM</td>
+                </tr>
+                <tr>
+                  <td>SUNDAY</td>
+                  <td>CLOSED</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            229 Skokie Valley Road. Highland Park, IL 60035 / suite 24 <br />
+            Phone: (847) 777-0274
+            <br />
+            <button id="btn">Send us a Message</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- client/src/components/services/index.js
+++ client/src/components/services/index.js
@@ -1,6 +1,43 @@
 import React from "react";
 import "./style.css";
+import { Jumbotron as Jumbo } from "react-bootstrap";
+import Background from "../../images/Rectangle115.jpg";
+import styled from "styled-components";
+
+// const Styles = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: `url(${Background})`
+// };
+
+const Styles = styled.div`
+  .jumbotron {
+    background: url(${Background}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #ccc;
+    height: 200px;
+    position: relative;
+    z-index: -2;
+  }
+
+  .overlay {
+    background-color: #000;
+    opacity: 0.8;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -3;
+  }
+`;
 
 export const Services = () => {
-  return <h1>Hello Services</h1>;
+  return (
+    <Styles>
+      <Jumbo fluid className="jumbo">
+        <div className="overlay"></div>
+      </Jumbo>
+    </Styles>
+  );
 };
